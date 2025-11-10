5254f055ece23e11c81613f764d2b89a53545620
parvinghasemi <mghasemi.parvin@gmail.com>
2025-11-10 14:38:33 +0100

    the skeleton of variadic functions in ft_printf

--- ft_printf.h
+++ ft_printf.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: pamohamm <pamohamm@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/08 16:56:15 by pamohamm          #+#    #+#             */
-/*   Updated: 2025/11/08 20:13:43 by pamohamm         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   ft_printf.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pamohamm <pamohamm@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/11/08 16:56:15 by pamohamm      #+#    #+#                 */
+/*   Updated: 2025/11/10 14:24:26 by parvinm.gha   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdarg.h>
 #include "libft/libft.h"
 
 int	ft_printf(const	char *, ...);

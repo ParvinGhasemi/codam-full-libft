ea065363bcc36205c3d13ac7c20eb6f039c7b07f
parvinghasemi <mghasemi.parvin@gmail.com>
2025-11-11 21:16:42 +0100

    char_handler.c: added print_str()

--- char_handler.c
+++ char_handler.c
@@ -5,3 +5,20 @@ int	print_character(char c)
 {
 	return ((int)write(1, &c, 1));
 }
+
+int	print_string(char *str)
+{
+	int	len;
+
+	if (str == NULL)
+		str = "(null)";
+	len = (int)ft_strlen(str);
+	return ((int)write(1, str, len));
+}
+
+// int main()
+// {
+// 	char *str = NULL;
+// 	printf("%s\n", str);
+// 	print_string(str);
+// }
